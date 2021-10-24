d6510fcb74f4528434711144d23b8e9c10343512
souheyeb <souheyeb@gmail.com>
2021-10-24 21:59:53 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+   $valeur = $_POST;
+   if(!empty($valeur)){
+  
+};
+   
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login</title>
+  </head>
+  <body>
+    <h1>Formulaire de login</h1>
+    <form action="./index.php" method="post">
+      <label for="login">Login</label>
+      <input value="" name="login" /><br />
+      <label for="password">Password</label>
+      <input value="" name="password" />
+      <input type="submit" />
+    </form>
+  </body>
+</html>
